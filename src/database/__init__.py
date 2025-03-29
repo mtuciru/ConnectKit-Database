d8d93146b3d0b4d3043e23e9656ccefed7e17fbf
RealMetamorph <andr.timchuk@yandex.ru>
2025-03-29 21:07:31 +0300

    add __all__ in init files

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -2,7 +2,10 @@ from sqlalchemy.orm import declarative_base, Session
 import database.errors as errors
 from database.session import Database
 from database.init_base import init_default_base
-from database.asyncio import AsyncDatabase, AsyncSession, async_init_default_base
+from database import asyncio
+from database.asyncio import *
 from database.settings import Adapter, settings
 
 Base = declarative_base()
+
+__all__ = ["Database", "Session", "asyncio", "init_default_base", "Base", "Adapter", "settings", "errors"]
--- src/database/asyncio/__init__.py
+++ src/database/asyncio/__init__.py
@@ -1,3 +1,5 @@
 from database.asyncio.session import AsyncDatabase, get_default_async_engine
 from database.asyncio.init_base import async_init_default_base
 from sqlalchemy.ext.asyncio import AsyncSession
+
+__all__ = ["AsyncDatabase", "AsyncSession", 'async_init_default_base']
